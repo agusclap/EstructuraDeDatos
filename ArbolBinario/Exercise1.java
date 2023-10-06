481a461641c88cd630f04597bacd0c09f8ee5371
agusclap <rodeyroagustin@gmail.com>
2023-10-06 10:13:07 -0300

    modified

--- ArbolBinario/Exercise1.java
+++ ArbolBinario/Exercise1.java
@@ -2,21 +2,21 @@ package ArbolBinario;
 
 import java.util.Random;
 
-public class Ejercicio1 {
+public class Exercise1 {
 
     
     public static void main(String[] args) {
         
         Random random = new Random();
-        BinarySearchTree<Integer> arbol = new BinarySearchTree<Integer>();
+        BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
         for(int i=0;i<10;i++){
             int n = random.nextInt(25);
-            arbol.insert(n);
+            tree.insert(n);
         }
-        System.out.println(arbol.getRoot());
-        arbol.printPreOrder();  // Imprime los numeros de forma ordenada (Ejercicio 2)
-        System.out.println(arbol.height());
-        System.out.println(arbol.size());
+        System.out.println(tree.getRoot()); // Return tree's root
+        tree.printPreOrder();  // print the numbers in ordered form ( Exercise 2 )
+        System.out.println(tree.height()); // Height ( levels )
+        System.out.println(tree.size()); // tree's size ( amount of nodes entered )
     }
-    
+    // All the modifications of practice five are done.
 }
